7e1ba61e7e08209b2f3c95630e7809461e33e855
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2021-12-24 18:07:58 -0300

    Adicionando Spring Boot Tools

---
+++ Pratica-2/src/main/java/br/com/prog3/Pratica2/Pratica2Application.java
@@ -0,0 +1,13 @@
+package br.com.prog3.Pratica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratica2Application.class, args);
+	}
+
+}
